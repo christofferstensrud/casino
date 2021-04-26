9bea4650c7fba9a6b60b2c07a7c6e1ab0ddc2347
Christoffer Stensrud <christofferstensrud@gmail.com>
2021-04-26 22:52:39 +0200

    reconfigure BELL

--- src/main/java/casino/games/slot/SlotDefaults.java
+++ src/main/java/casino/games/slot/SlotDefaults.java
@@ -8,7 +8,7 @@ public class SlotDefaults {
     private static final SlotSymbol CHERRY = new SlotSymbol("Cherry", "/images/CHERRY.PNG",4, true);
     private static final SlotSymbol LEMON = new SlotSymbol("Lemon", "/images/LEMON.PNG",4, true);
     private static final SlotSymbol MELON = new SlotSymbol("Melon", "/images/MELON.PNG",4, true);
-    private static final SlotSymbol BELL = new SlotSymbol("Bell","/images/BELL.PNG",10, true);
+    private static final SlotSymbol BELL = new SlotSymbol("Bell","/images/BELL.PNG",10, false);
     private static final SlotSymbol SEVENS = new SlotSymbol("Seven", "/images/SEVENS.PNG",17, false);
     private static final SlotSymbol BAR = new SlotSymbol("Bar", "/images/BAR.PNG",50, false);
 
